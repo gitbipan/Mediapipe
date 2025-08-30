b20a28576da39650ed2a5eabd6d9562591d2de0c
LedBipan <shresthabro.77.f@gmail.com>
2025-08-30 19:06:03 +0545

    added modules

--- pyserial.py
+++ pyserial.py
@@ -1,1 +1,10 @@
-import pyserial as ps
+import serial as ser
+import cv2
+import mediapipe as mp
+
+webcam=cv2.VideoCapture(0)
+mp_face=mp.solutions.face_mesh
+mp_drawing=mp.solutions.drawing_utils
+arduino=ser.Serial('com8',9600)
+# necessary modules and variable
+    
